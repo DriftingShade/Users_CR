6154b21ef8efc40e52413a191ddbd17ca8d89d76
DriftingShade <sarus333@yahoo.com>
2024-03-09 16:41:28 -0700

    work in progress, I'm lost

--- server.py
+++ server.py
@@ -4,29 +4,40 @@ from user import User
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/create_user', methods=["POST"])
+
+@app.route("/create_user", methods=["POST"])
 def create_user():
 
     data = {
         "fname": request.form["fname"],
         "lname": request.form["lname"],
-        "email": request.form["email"]
+        "email": request.form["email"],
     }
     User.save(data)
-    return redirect("/users")
+    return redirect("/show_user")
 
-@app.route('/users')
+
+@app.route("/users")
 def show_users():
     users = User.get_all()
 
-    return render_template("users.html", all_users = users)
+    return render_template("users.html", all_users=users)
 
 
+@app.route("/edit_user")
+def edit_user():
+    User.update(request.form)
+    return redirect("/show_user")
 
+
+@app.route("/show_user")
+def one_user():
+    return render_template("single_user.html")
 
 
 if __name__ == "__main__":
